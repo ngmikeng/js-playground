09f415fcafa3c338dcc9eb9af1ffffcdbd89cbd2
Minh Nguyen <ngmikeng@gmail.com>
2018-06-03 23:58:34 +0700

    WIP practise data structures.

---
+++ data-structures/js/array.js
@@ -0,0 +1,12 @@
+(function() {
+  function addAtBegin(inputArray, newItem) {
+    var length = inputArray.length;
+    for (var i = length; i >= 0; --i) {
+      inputArray[i] = inputArray[i - 1];
+    }
+    inputArray[0] = newItem;
+    console.log(inputArray); // 1,2,3,4,5
+  }
+
+  addAtBegin([1,2,3,4,5], 10);
+})();
